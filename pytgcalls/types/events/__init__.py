dff023988476a02cd21da9849b8bbf0b9aaed082
ꜱᴜᴋʜ <123292087+Badmunda50@users.noreply.github.com>
2025-05-28 11:01:38 +0530

    Create __init__.py

---
+++ pytgcalls/types/events/__init__.py
@@ -0,0 +1,7 @@
+from .group_call_participant_joined import GroupCallParticipantJoined
+from .group_call_participant_left import GroupCallParticipantLeft
+
+__all__ = [
+    "GroupCallParticipantJoined",
+    "GroupCallParticipantLeft"
+]
